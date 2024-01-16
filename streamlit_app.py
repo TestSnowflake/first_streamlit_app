b7b36982942c5b26e356ac0f0aff2d62f23085df
TestSnowflake <156814259+TestSnowflake@users.noreply.github.com>
2024-01-16 15:42:04 +0100

    Update streamlit_app.py
    Add st Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+st.text( 'Omega 3 & Blueberry Oatmeal')
+st.text( 'Kale, Spinach & Rocket Smoothie')
+st.text( 'Hard6Boiled Free Range Egg')
+

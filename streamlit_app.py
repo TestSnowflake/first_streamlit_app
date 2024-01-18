33728a6060fe80abd706350f2cfdc98fd6a21cd8
TestSnowflake <156814259+TestSnowflake@users.noreply.github.com>
2024-01-18 10:44:09 +0100

    Update streamlit_app.py
    modification pour corriger l'erreur

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,11 +16,9 @@ my_first_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_first_list = my_first_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect ("Pick some fruits:", list(my_first_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_S = streamlit.multiselect ("Pick some fruits:", list(my_first_list.index),['Avocado','Strawberries'])
+fruitsToShow= my_fruit_list.loc[fruits_S]
 #Display the table
-#streamlit.dataframe(my_first_list)
-
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruitsToShow)
 
 

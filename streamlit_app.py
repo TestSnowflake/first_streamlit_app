5e24feb88f464b3bf2b182139a7ca822d6bdafee
TestSnowflake <156814259+TestSnowflake@users.noreply.github.com>
2024-03-04 10:34:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_To_Show)
 
 streamlit.header('Fruityvice Fruit Advice !')
 fruityvice_response= requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 

323dadf7e41f71539ed0b27c15f29fff93289211
TestSnowflake <156814259+TestSnowflake@users.noreply.github.com>
2024-03-04 10:28:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import requets
+import requests
 streamlit.title('My Mom s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text( '🥣 Omega 3 & Blueberry Oatmeal')

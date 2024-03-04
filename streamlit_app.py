c589d4a5388ef3efaf3f1131d2ebbb2099bb7a8b
TestSnowflake <156814259+TestSnowflake@users.noreply.github.com>
2024-03-04 10:40:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_To_Show)
 
 
 streamlit.header('Fruityvice Fruit Advice !')
-Fruit_choice= streamlit.text_Input('what fruit would you like information about?','kiwi')
-streamlit.write('The user entered',Fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 
 fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")

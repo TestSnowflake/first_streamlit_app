ee47ee853cfb2f0085bd5f4cec817865ae8f27f2
TestSnowflake <156814259+TestSnowflake@users.noreply.github.com>
2024-03-04 10:21:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,5 +26,5 @@ streamlit.dataframe(fruits_To_Show)
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized
+streamlit.dataframe(fruityvice_normalized) 
 
